364ea0c007d5323507963822c1c8f3297a4b9fbd
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-11 09:55:02 +0200

    Nueva subida de datos

--- pre/js/main.js
+++ pre/js/main.js
@@ -1,7 +1,6 @@
 //Desarrollo de la visualización
 import * as d3 from 'd3';
 import * as topojson from "topojson-client";
-let d3_composite = require("d3-composite-projections");
 
 //Necesario para importar los estilos de forma automática en la etiqueta 'style' del html final
 import '../css/main.scss';
@@ -14,18 +13,18 @@ const height = parseInt(map1.style('height'));
 
 let mapLayer1 = map1.append('svg').attr('width', width).attr('height', height),
     mapLayer2 = map2.append('svg').attr('width', width).attr('height', height);
-    let distritos;
+let distritos;
 let projection, path;
 
 d3.queue()
-    .defer(d3.json, 'https://raw.githubusercontent.com/carlosmunozdiaz/simple_covid19_madrid_map/main/data/distritos_topo.json')
+    .defer(d3.json, 'https://raw.githubusercontent.com/carlosmunozdiaz/simple_covid19_madrid_map/main/data/distritos_v2.json')
     .defer(d3.csv, 'https://raw.githubusercontent.com/carlosmunozdiaz/simple_covid19_madrid_map/main/data/covid19_anni.csv')
     .await(main);
 
 function main(error, distritosAux, data) {
     if (error) throw error;
 
-    distritos = topojson.feature(distritosAux, municipios.objects.distritos);
+    distritos = topojson.feature(distritosAux, distritosAux.objects.distritos);
 
     console.log(data);
 
@@ -39,75 +38,17 @@ function main(error, distritosAux, data) {
     //     join = join[0];
     //     item.data = join;
     // });
-
-    projection = d3_composite.geoConicConformalSpain().scale(2000).fitSize([width,height], distritos);
+    projection = d3.geoMercator().center([-3.816,40.528]).translate([width/2,height/3.5]),
     path = d3.geoPath(projection);
 
-    mapLayer.selectAll(".dist")
+    console.log(distritos);
+
+    mapLayer1.selectAll(".dist1")
         .data(distritos.features)
         .enter()
         .append("path")
-        .attr("class", "dist")
-        .style('stroke','none')
+        .attr("class", "dist1")
+        .style('stroke','blue')
         .style('opacity', '1')
-        .style('fill', function(d) {
-            if(d.data) {
-                if (d.data.porc_envejecido != 'NA') {
-                    let color = '';
-                    let env = +d.data.porc_envejecido.replace(',','.');
-                    let total = +d.data.total;
-
-                    if ( total < 1000) {
-                        if (env < 15) {
-                            color = '#e8e8e8';
-                        } else if (env >= 15 && env < 30) {
-                            color = '#b5c0da';
-                        } else {
-                            color = '#6c83b5';
-                        }
-                    } else if ( total >= 1000 && total < 20000) {
-                        if (env < 15) {
-                            color = '#b8d6be';
-                        } else if (env >= 15 && env < 30) {
-                            color = '#8fb2b3';
-                        } else {
-                            color = '#567994';
-                        }
-                    } else {
-                        if (env < 15) {
-                            color = '#73ae7f';
-                        } else if (env >= 15 && env < 30) {
-                            color = '#5a9178';
-                        } else {
-                            color = '#2b5a5b';
-                        }
-                    }
-
-                    return color;
-
-
-                } else {
-                    return '#ccc';
-                }                
-            } else {
-                return '#ccc';
-            }            
-        })
         .attr("d", path);
-
-    mapLayer.append('path')
-        .style('fill', 'none')
-        .style('stroke', '#000')
-        .attr('d', projection.getCompositionBorders());
-
-    mapLayer.selectAll('.prov')
-        .data(provs.features)
-        .enter()
-        .append('path')
-        .attr('d', path)
-        .style('stroke-width','0.25px')
-        .style('stroke', '#000')
-        .style('fill', 'transparent');
-
-    setChartCanvas();
 }
