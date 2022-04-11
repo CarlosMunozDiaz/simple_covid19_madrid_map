4c0373763cccfb88c22fcba3b477495d154410d1
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-11 10:07:23 +0200

    Primer desarrollo del mapa simple

--- pre/js/main.js
+++ pre/js/main.js
@@ -1,6 +1,7 @@
 //Desarrollo de la visualización
 import * as d3 from 'd3';
 import * as topojson from "topojson-client";
+let d3_composite = require("d3-composite-projections");
 
 //Necesario para importar los estilos de forma automática en la etiqueta 'style' del html final
 import '../css/main.scss';
@@ -26,29 +27,62 @@ function main(error, distritosAux, data) {
 
     distritos = topojson.feature(distritosAux, distritosAux.objects.distritos);
 
-    console.log(data);
-
     ///HACEMOS EL JOIN
-    // muni.features.forEach(function(item) {
-    //     let join = data.filter(function(subItem) {
-    //         if(subItem.Municipios.substr(0,5) == item.properties.Codigo) {
-    //             return subItem;
-    //         }
-    //     });
-    //     join = join[0];
-    //     item.data = join;
-    // });
-    projection = d3.geoMercator().center([-3.816,40.528]).translate([width/2,height/3.5]),
-    path = d3.geoPath(projection);
+    distritos.features.forEach(function(item) {
+        let join = data.filter(function(subItem) {
+            if(subItem.municipio_distrito.substr(7) == item.properties.NOMBRE) {
+                return subItem;
+            }
+        });
+        item.data = join;
+    });
 
-    console.log(distritos);
+    projection = d3_composite.geoConicConformalSpain().scale(2000).fitSize([width,height], distritos);
+    path = d3.geoPath(projection);
 
     mapLayer1.selectAll(".dist1")
         .data(distritos.features)
         .enter()
         .append("path")
         .attr("class", "dist1")
-        .style('stroke','blue')
+        .style('stroke','#262626')
+        .style('stroke-width','0.6px')
         .style('opacity', '1')
+        .style("fill", function(d) {
+            if(parseInt(d.data[0].casos_totales) >= 2600 & parseInt(d.data[0].casos_totales) < 7480) {
+                return '#ffe6b7';
+            } else if (parseInt(d.data[0].casos_totales) >= 7480 & parseInt(d.data[0].casos_totales) < 12360) {
+                return '#fecc7b';
+            } else if (parseInt(d.data[0].casos_totales) >= 12360 & parseInt(d.data[0].casos_totales) < 17240) {
+                return '#f8b05c';
+            } else if (parseInt(d.data[0].casos_totales) >= 17240 & parseInt(d.data[0].casos_totales) < 22120) {
+                return '#f1944d';
+            } else if (parseInt(d.data[0].casos_totales) >= 22120) {
+                return '#e37a42';
+            }
+        })
+        .attr("d", path);
+
+    mapLayer2.selectAll(".dist2")
+        .data(distritos.features)
+        .enter()
+        .append("path")
+        .attr("class", "dist2")
+        .style('stroke','#262626')
+        .style('stroke-width','0.6px')
+        .style('opacity', '1')
+        .style("fill", function(d) {
+            if(parseInt(d.data[1].casos_totales) >= 2600 & parseInt(d.data[1].casos_totales) < 7480) {
+                return '#ffe6b7';
+            } else if (parseInt(d.data[1].casos_totales) >= 7480 & parseInt(d.data[1].casos_totales) < 12360) {
+                return '#fecc7b';
+            } else if (parseInt(d.data[1].casos_totales) >= 12360 & parseInt(d.data[1].casos_totales) < 17240) {
+                return '#f8b05c';
+            } else if (parseInt(d.data[1].casos_totales) >= 17240 & parseInt(d.data[1].casos_totales) < 22120) {
+                return '#f1944d';
+            } else if (parseInt(d.data[1].casos_totales) >= 22120) {
+                return '#e37a42';
+            }
+        })
         .attr("d", path);
 }
